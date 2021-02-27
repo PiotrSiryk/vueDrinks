4ace77ef92eb8efaa2a37051974c1252573483d4
PiotrSiryk <Piotr.Siryk@gmail.com>
2021-02-27 19:53:02 +0100

    setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,40 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 1551,
+    secondCount: 5,
+    arr: [1, 2, 3, 4, 5],
   },
   mutations: {
+    increment: (state) => state.secondCount++,
+    addToArr: (state, item) => {
+      return state.arr.push(item);
+    },
   },
   actions: {
+    fetchStuff() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((data) => console.log(data.data));
+    },
+    add() {
+      this.commit("increment");
+    },
+    addToList({ commit }, data) {
+      commit("addToArr", data);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    summary(state) {
+      console.log(state);
+      return state.count + state.secondCount;
+    },
+    printArr: (state) => state.arr,
+  },
+});
